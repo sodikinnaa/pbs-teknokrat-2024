6efcb53215ae74e164c6605be36ad2f08622e49f
sodikinnaa <sodikinnaa@gmail.com>
2024-03-31 13:46:12 +0700

    add konfig vercel.json

--- connection.js
+++ connection.js
@@ -2,9 +2,9 @@ const mysql = require("mysql");
 
 const db = mysql.createConnection({
   host: "sql6.freesqldatabase.com",
-  user: "sql6695511",
-  database: "sql6695511",
-  password: "Naj8NV1HXi",
+  user: "sql6695515",
+  database: "sql6695515",
+  password: "KfvpfyPLZZ",
 });
 
 module.exports = db;
